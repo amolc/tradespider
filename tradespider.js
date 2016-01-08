dc77ee226ea3efc08729db5c22c0e4dbba04f9ec
ftechiesnitin <nitin.dolli@fountaintechies.com>
2016-01-08 14:05:09 +0530

    express and port added

--- tradespider.js
+++ tradespider.js
@@ -1,6 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require("http").createServer(app);
+// var io = require("./socket/socket");
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var CRUD = require('mysql-crud');
 var request = require('request');
 var cheerio = require('cheerio');
 var cron = require('cron');
+// var neuron = require('neuron');
+
+var dbConnection = mysql.createPool({
+  database : 'tradespider',
+  user : 'ftdev',
+  password : '10gXWOqeaf',
+  host :'apps.fountaintechies.com',
+});
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use('/', express.static(__dirname + '/web'));
+// app.get('/dax_data', function (req, res) {
+//   console.log('data received');
+// });
 
 var one_minute = {
    url : 'http://www.investing.com/indices/germany-30-technical?period=60',
@@ -18,27 +39,32 @@ var five_minute = {
 
 // One Minute Cron
 var oneMinuteCron = cron.job('0 * * * * *', function(){
+
   request(one_minute, function (error, response, html) {
     if (!error && response.statusCode == 200) {
       console.log('----------- ONE MINUTE SUMMARY ----------');
       var $ = cheerio.load(html);
-      var parsedResults = [];
+      var oneMinuteData = {};
       $('div #techStudiesInnerWrap').each(function(i, element){
-        console.log($(this).children('.summary').text());
 
+        // Summary Info
+        oneMinuteData.summary = $(this).children('.summary').children('span').text();
+
+        // Moving Averages Info
         if($(this).children('.summaryTableLine').children('span').eq(1).children('span').length === 1){
-          console.log($(this).children('.summaryTableLine').children('span').eq(0).text() + ' ' + $(this).children('.summaryTableLine').children('span').eq(1).children('span').text());
+          oneMinuteData.moving_averages = $(this).children('.summaryTableLine').children('span').eq(1).children('span').text();
         }else if($(this).children('.summaryTableLine').children('span').eq(1).children('b').length === 1){
-          console.log($(this).children('.summaryTableLine').children('span').eq(0).text() + ' ' + $(this).children('.summaryTableLine').children('span').eq(1).children('b').text());
+          oneMinuteData.moving_averages = $(this).children('.summaryTableLine').children('span').eq(1).children('b').text();
         }
 
+        // Technical Indicators Info
         if($(this).children('.summaryTableLine').next().children('span').eq(1).children('span').length === 1){
-          console.log($(this).children('.summaryTableLine').next().children('span').eq(0).text() + ' '+ $(this).children('.summaryTableLine').next().children('span').eq(1).children('span').text());
+          oneMinuteData.technical_indicators = $(this).children('.summaryTableLine').next().children('span').eq(1).children('span').text();
         }else if($(this).children('.summaryTableLine').next().children('span').eq(1).children('b').length === 1){
-          console.log($(this).children('.summaryTableLine').next().children('span').eq(0).text() + ' '+ $(this).children('.summaryTableLine').next().children('span').eq(1).children('b').text());
+          oneMinuteData.technical_indicators = $(this).children('.summaryTableLine').next().children('span').eq(1).children('b').text();
         }
-
       });
+      console.log(oneMinuteData);
     }
   });
 
@@ -46,25 +72,34 @@ var oneMinuteCron = cron.job('0 * * * * *', function(){
     if (!error && response.statusCode == 200) {
       console.log('----------- FIVE MINUTE SUMMARY ----------');
       var $ = cheerio.load(html);
-      var parsedResults = [];
+      var fiveMinuteData = {};
       $('div #techStudiesInnerWrap').each(function(i, element){
-        console.log($(this).children('.summary').text());
 
+        // Summary Info
+        fiveMinuteData.summary = $(this).children('.summary').children('span').text();
+
+        // Moving Averages Info
         if($(this).children('.summaryTableLine').children('span').eq(1).children('span').length === 1){
-          console.log($(this).children('.summaryTableLine').children('span').eq(0).text() + ' ' + $(this).children('.summaryTableLine').children('span').eq(1).children('span').text());
+          fiveMinuteData.moving_averages = $(this).children('.summaryTableLine').children('span').eq(1).children('span').text();
         }else if($(this).children('.summaryTableLine').children('span').eq(1).children('b').length === 1){
-          console.log($(this).children('.summaryTableLine').children('span').eq(0).text() + ' ' + $(this).children('.summaryTableLine').children('span').eq(1).children('b').text());
+          fiveMinuteData.moving_averages = $(this).children('.summaryTableLine').children('span').eq(1).children('b').text();
         }
 
+        // Technical Indicators Info
         if($(this).children('.summaryTableLine').next().children('span').eq(1).children('span').length === 1){
-          console.log($(this).children('.summaryTableLine').next().children('span').eq(0).text() + ' '+ $(this).children('.summaryTableLine').next().children('span').eq(1).children('span').text());
+          fiveMinuteData.technical_indicators = $(this).children('.summaryTableLine').next().children('span').eq(1).children('span').text();
         }else if($(this).children('.summaryTableLine').next().children('span').eq(1).children('b').length === 1){
-          console.log($(this).children('.summaryTableLine').next().children('span').eq(0).text() + ' '+ $(this).children('.summaryTableLine').next().children('span').eq(1).children('b').text());
+          fiveMinuteData.technical_indicators = $(this).children('.summaryTableLine').next().children('span').eq(1).children('b').text();
         }
       });
+      console.log(fiveMinuteData);
     }
   });
 
 });
 
 oneMinuteCron.start();
+
+
+http.listen(5555);
+console.log("listening to port 5555");
