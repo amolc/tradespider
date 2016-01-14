84570fce9753798db4e1d80ae31e6fc2c9baefba
ftechiesnitin <nitin.dolli@fountaintechies.com>
2016-01-14 17:51:19 +0530

    local config canged

--- web/js/config.js
+++ web/js/config.js
@@ -1,9 +1,7 @@
 var instance = "dev";
 
 if (instance == "dev"){
-	var baseURL="http://188.166.216.245:5555/api/";
-	var socketUrl = 'http://188.166.216.245:5555';
+	var socketUrl = 'http://tradealert.fountaintechies.com:5555/';
 }else if(instance == "local"){
-	var baseURL="http://localhost:5555/api/";
 	var socketUrl = 'http://localhost:5555';
 }
