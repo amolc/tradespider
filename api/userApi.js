e05228dc7682774111e57dbb17205423d44a24d4
mandar0119 <mandar.joshi@fountaintechies.com>
2016-03-24 14:47:00 +0530

    minor console req.body for device

--- api/userApi.js
+++ api/userApi.js
@@ -1,9 +1,8 @@
 var notification = require('mongoose').model('notification');
 var commonFunctions = require('./functions');
 exports.addDevice = function (req, res) {
-	
+	console.log('req.body',req.body);
 	var deviceData = new notification(req.body);
-
 	deviceData.save(function(err) {
 	  if (err) {
 	    console.log(err);
