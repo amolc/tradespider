5611c08f28607712e4a62c905162478403dd4191
mandar0119 <mandar.joshi@fountaintechies.com>
2016-03-29 14:48:50 +0530

    database

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://trade:3cxWOqeaf@localhost:27017/tradespider');
+// mongoose.connect('mongodb://trade:3cxWOqeaf@localhost:27017/tradespider');
+mongoose.connect('mongodb://localhost/tradespider');
 
 require('../schema/table.schema');
 require('../schema/tableNotificationSchema');
