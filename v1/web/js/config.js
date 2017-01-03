a97e1bdfc6b63c73a28dd39e250fb10be648c7d3
Nitin Dolli <nitin.dolli@gmail.com>
2017-01-03 15:37:30 +0800

    web config updated

--- v1/web/js/config.js
+++ v1/web/js/config.js
@@ -1,7 +1,7 @@
 var instance = "dev";
 
 if (instance == "dev"){
-	var socketUrl = 'http://tradealert.fountaintechies.com:5555';
+	var socketUrl = 'http://n1.gigstergo.com:5555';
 }else if(instance == "local"){
 	var socketUrl = 'http://localhost:5555';
 }
